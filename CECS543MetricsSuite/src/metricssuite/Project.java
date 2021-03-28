92d28fdbde4e96a407c0c9fe8da5f8ff6af83ca1
LilibethBlandon <31785850+LilibethBlandon@users.noreply.github.com>
2021-03-27 21:48:42 -0700

    Iteration1
    GUI, function points w/ code sizing.

---
+++ CECS543MetricsSuite/src/metricssuite/Project.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metricssuite;
+
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author school
+ */
+public class Project {
+    String projectName, productName, creator, comments;
+    //ArrayList<JPanel> jPanels = new ArrayList<>();
+    String language = "";
+    ArrayList<Pane> projectPanes = new ArrayList<>();
+    
+    public Project(String projectName, String productName, String creator, String comments, String language){
+        this.projectName = projectName;
+        this.productName = productName;
+        this.creator = creator;
+        this.comments = comments;
+        this.language = language;
+    }
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public ArrayList<Pane> getProjectPanes() {
+        return projectPanes;
+    }
+
+    public void setProjectPanes(ArrayList<Pane> projectPanes) {
+        this.projectPanes = projectPanes;
+    }
+    
+    public void addProjectPane(Pane pane) {
+        projectPanes.add(pane);
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" + "projectName=" + projectName + ", productName=" + productName + ", creator=" + creator + ", comments=" + comments + ", language=" + language + '}';
+    }
+    
+
+    
+    
+    
+    
+}
